6b010a5d17ede7e03f4662b8e2ce6c290db4b773
Julia Costa <juliacosta232005@gmail.com>
2025-12-05 23:56:04 +0000

    Simple Script for Multiple Players Movin

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _velocity;
+    [SerializeField] private int _playerNum;
+    private Rigidbody _rigidbody;
+    private Vector2 _moveDelta = Vector2.zero;
+    private Vector3 _baseVelocity = Vector2.zero;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        DoMovement();
+
+        _rigidbody.linearVelocity = _baseVelocity;
+    }
+
+    private void DoMovement()
+    {
+        _moveDelta.x = Input.GetAxis("Horizontal" + _playerNum);
+        _moveDelta.y = Input.GetAxis("Vertical" + _playerNum);
+
+        // Calculate base velocity from input
+        _baseVelocity.x = _velocity * _moveDelta.x;
+        _baseVelocity.y = _velocity * _moveDelta.y;
+    }
+}
