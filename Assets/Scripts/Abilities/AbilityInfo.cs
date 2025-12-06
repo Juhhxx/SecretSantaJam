7b2349f4a278c9aaebbc85f6919d68d9129ea7f8
Julia Costa <juliacosta232005@gmail.com>
2025-12-06 20:49:09 +0000

    Things

--- Assets/Scripts/Abilities/AbilityInfo.cs
+++ Assets/Scripts/Abilities/AbilityInfo.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AbilityInfo", menuName = "Scriptable Objects/AbilityInfo")]
 public class AbilityInfo : ScriptableObject
 {
-    
+    [SerializeField] private GameObject _abilityPrefab;
+
+    public void SpawnAbility(Vector3 position) => Instantiate(_abilityPrefab, position, Quaternion.identity);
 }
